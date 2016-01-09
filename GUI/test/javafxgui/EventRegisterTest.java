7a219b37be176122e1acc932bbdfc1fbd35c3855
sarahabimay <sarahabimay@gmail.com>
2016-01-09 17:21:56 +0000

    Fix broken tests that somehow slipped my notice.

--- GUI/test/javafxgui/EventRegisterTest.java
+++ GUI/test/javafxgui/EventRegisterTest.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafxgui.event.EventRegister;
 import javafxgui.view.BoardDisplay;
+import javafxgui.view.GUIView;
 import jttt.Core.Board.Board;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,27 +20,32 @@ public class EventRegisterTest {
     private EventRegister eventRegister;
     private Scene scene;
     private GUIViewSpy guiViewSpy;
+    private final int DEFAULT_WINDOW_WIDTH = 600;
+    private final int DEFAULT_WINDOW_HEIGHT = 700;
+    private GUIView guiView;
 
     @Before
     public void setUp() {
         new JFXPanel();
         eventRegister = new EventRegister();
-        guiViewSpy = new GUIViewSpy(new Scene(new StackPane(), 700, 600), new BoardDisplay(), new EventRegister());
+        guiViewSpy = new GUIViewSpy(new Scene(new StackPane(), DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_HEIGHT), new BoardDisplay(), eventRegister);
+        guiView = new GUIView(new Scene(new StackPane(), DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_HEIGHT), new BoardDisplay(), eventRegister);
     }
 
     @Test
     public void registerAllBoardButtonsWithHandler() {
-        scene = guiViewSpy.displayGameLayoutComponent(new Board(3));
+        scene = guiView.displayGameLayoutComponent(new Board(3));
         GridPane gameBoard = (GridPane) scene.lookup("#gameBoard");
-        Button button = (Button) gameBoard.getChildren().get(0);
         eventRegister.registerAllBoardButtonsWithHandler(gameBoard, guiViewSpy);
+
+        Button button = (Button) gameBoard.getChildren().get(0);
         button.fire();
         assertEquals(true, guiViewSpy.hasBoardButtonBeenClicked());
     }
 
     @Test
     public void registerReplayButtonWithHandler() {
-        scene = guiViewSpy.displayGameLayoutComponent(new Board(3));
+        scene = guiView.displayGameLayoutComponent(new Board(3));
         Button replayButton = (Button) scene.lookup("#playAgain");
         eventRegister.registerAllClickableElementsWithHandler(scene, guiViewSpy);
         replayButton.fire();
@@ -48,7 +54,7 @@ public class EventRegisterTest {
 
     @Test
     public void registerAllClickableElementsWithHandlers() {
-        scene = guiViewSpy.displayGameLayoutComponent(new Board(3));
+        scene = guiView.displayGameLayoutComponent(new Board(3));
         eventRegister.registerAllClickableElementsWithHandler(scene, guiViewSpy);
 
         GridPane gameBoard = (GridPane) scene.lookup("#gameBoard");
